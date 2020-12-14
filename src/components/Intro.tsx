b6325e1875766710e34a4391ed3217862ff047be
Vinnie Rossi <VinnieRossi@users.noreply.github.com>
2020-12-13 22:06:56 -0500

    Update Intro.tsx

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -1,71 +1,7 @@
-import { Link } from "@material-ui/core";
-import React from "react";
 import "tailwindcss/tailwind.css";
 
 const Intro = (): JSX.Element => {
   return (
-    // <div classNameNameName="">
-    //   <div classNameNameName="container mx-auto p-4">
-    //     <h1 classNameNameName="text-4xl pb-4 font-semibold">Don't time the market!</h1>
-    //     <h2 classNameNameName="text-2x1 pb-4">And other things to know</h2>
-    //   </div>
-
-    //   <div classNameNameName="bg-gray-100">
-    //     <div classNameNameName="container grid grid-cols-2 grid-rows-2 mx-auto p-4">
-    //       <div classNameNameName="p-2">
-    //         <p>
-    //           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam sed
-    //           est vel odio egestas rhoncus nec at nisi. Aenean feugiat elit nec
-    //           bibendum facilisis. Sed vehicula fringilla varius. Praesent
-    //           interdum pellentesque eros id ultrices. In hac habitasse platea
-    //           dictumst. Phasellus rhoncus sagittis pretium. Maecenas tempus est
-    //           nec sapien feugiat interdum. Nulla egestas nunc in sapien
-    //           consectetur aliquet. Curabitur non neque ut felis faucibus
-    //           efficitur non id ligula.
-    //         </p>
-    //       </div>
-    //       <div classNameNameName="p-2">
-    //         <p>Put a picture here</p>
-    //       </div>
-    //       <div classNameNameName="p-2">
-    //         <p>Put a picture here</p>
-    //       </div>
-    //       <div classNameNameName="p-2">
-    //         <p>
-    //           Integer et iaculis nulla, quis hendrerit ante. Nunc iaculis ipsum
-    //           quis erat ullamcorper tristique. Nam eu magna at nunc porta
-    //           aliquet eget eget magna. Nullam id ipsum ac tellus dapibus
-    //           convallis vitae egestas lacus. Sed tincidunt luctus nisi sed
-    //           varius. Morbi ac interdum magna. Quisque lobortis nulla ante, quis
-    //           viverra dui dapibus nec. Vivamus nec lacinia sapien. Nullam
-    //           sagittis velit sit amet eros mollis, sed eleifend enim molestie.
-    //           Donec accumsan aliquet congue. Donec vulputate dolor nec risus
-    //           tincidunt, vitae sagittis libero sollicitudin. Nam finibus arcu
-    //           eget risus interdum auctor.
-    //         </p>
-    //       </div>
-    //     </div>
-    //   </div>
-
-    //   <div classNameNameName="container mx-auto p-4">
-    //     <div>
-    //       <Link href="/play">Try your luck</Link>
-    //     </div>
-    //   </div>
-
-    //   <div classNameNameName="bg-gray-100">
-    //     <div classNameNameName="container mx-auto p-4">
-    //       <p>Add footer?</p>
-    //     </div>
-    //   </div>
-
-    //   {/* <div classNameNameName="container mx-auto p-4">
-    //     <div>
-    //       <p>Words</p>
-    //     </div>
-    //   </div> */}
-    // </div>
-
     <div className="relative py-16 bg-white overflow-hidden">
       <div className="hidden lg:block lg:absolute lg:inset-y-0 lg:h-full lg:w-full">
         <div
